b6bcb2b52d99ac4d34a82f486caf6def3762b50e
root <root@heracles.localdomain>
2021-12-08 17:02:32 -0800

    Hopefully the final fix for this but probably not

--- encoder/focal.c
+++ encoder/focal.c
@@ -180,14 +180,21 @@ float x264_focal_abs_distance(x264_float2_t mb_pos){
         delta.y = mb_point.y - focal_point.y;
         delta.z = mb_point.z - focal_point.z;
         return (float) sqrt( ( delta.x * delta.x ) + ( delta.y * delta.y ) + ( delta.z * delta.z ) );
+    } else {
+        // video is fisheye (spherical)
+        mb_point = x264_focal_getSpherePos_sphereInput(mb_pos);
     }
-    // video is fisheye (spherical)
-    mb_point = x264_focal_getSpherePos_sphereInput(mb_pos);
     float focal_mag = sqrt(pow(focal_point.x, 2) + pow(focal_point.y, 2) + pow(focal_point.z, 2));
     float mb_point_mag = sqrt(pow(mb_point.x, 2) + pow(mb_point.y, 2) + pow(mb_point.z,2));
+
+    if (mb_point_mag == 0) {
+        return thresh+1; // prevents the division by 0 if mb_point is zero. Instead returns a value which will fail the inequality with thresh
+    }
+
     float mb_dot_focal = (focal_point.x * mb_point.x) + (focal_point.y * mb_point.y) + (focal_point.z * mb_point.z);
     float angle = acosf(mb_dot_focal/ (focal_mag * mb_point_mag));
-    return sinf(angle/4) * 2;
+    return sinf(angle/4) * 2; // provides a conversion of the angle between mb_pos and focal_pos to be compaired to thresh to see if it's in FOV
+    
 }
 
 x264_float3_t x264_focal_float3_normalize( x264_float3_t vector ){
@@ -247,6 +254,7 @@ x264_float3_t x264_focal_getSpherePos_sphereInput(x264_float2_t mb_pos) {
         sphereCoords.x = 0;
         sphereCoords.y = 0;
         sphereCoords.z = 0;
+        printf("[FOCAL] mb_pos is outside of sphere video\n");
         return sphereCoords;
     }
 
