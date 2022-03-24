6eb50e34fe7eed821602ca2026c76cbc77a8adcf
root <root@arvrapp01.cc.net.rogers.com>
2022-03-24 18:19:14 -0400

    remake

--- x264_config.h
+++ x264_config.h
@@ -2,7 +2,7 @@
 #define X264_INTERLACED    1
 #define X264_BIT_DEPTH     0
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 0
-#define X264_REV_DIFF 83
-#define X264_VERSION " r0+83M 9a70b2a"
-#define X264_POINTVER "0.160.0+83M 9a70b2a"
+#define X264_REV 96
+#define X264_REV_DIFF 0
+#define X264_VERSION " r96M 3544d41"
+#define X264_POINTVER "0.160.96M 3544d41"
