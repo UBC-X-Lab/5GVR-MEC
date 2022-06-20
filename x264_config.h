beb0b61f982fe2d0b78a676c3019f1a5b4dec60c
Peter-Pater <xh721@nyu.edu>
2022-06-19 23:24:43 -0700

    new make

--- x264_config.h
+++ x264_config.h
@@ -3,6 +3,6 @@
 #define X264_BIT_DEPTH     0
 #define X264_CHROMA_FORMAT 0
 #define X264_REV 98
-#define X264_REV_DIFF 1
-#define X264_VERSION " r98+1M e9f970d"
-#define X264_POINTVER "0.160.98+1M e9f970d"
+#define X264_REV_DIFF 9
+#define X264_VERSION " r98+9M dffd287"
+#define X264_POINTVER "0.160.98+9M dffd287"
