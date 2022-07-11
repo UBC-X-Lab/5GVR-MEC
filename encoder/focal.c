00022f236bccd4c40e4cd9d728d991d62e992126
Peter-Pater <xh721@nyu.edu>
2022-07-10 20:13:01 -0400

    making foveation better

--- encoder/focal.c
+++ encoder/focal.c
@@ -119,7 +119,7 @@ int x264_focal_reallocate_qp( x264_t *h )
             input_data.status = uninitialized;
             input_data.x = 0;
             input_data.y = 0;
-            input_data.z = 1;
+            input_data.z = -1;
             if(x264_pthread_create(&connect_thread,NULL,x264_focal_connect,&input_data)){
                 printf("Focal failed to create thread for focal_connect\n");
             }
