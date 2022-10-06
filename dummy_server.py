745b880dbf51762147f14c73026e40d168c3464b
Peter-Pater <xh721@nyu.edu>
2022-10-06 14:30:52 -0700

    dummy server

---
+++ dummy_server.py
@@ -0,0 +1,31 @@
+import socket
+import struct
+import sys
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+IN_PORT = 27873 # video input port
+OUT_PORT = 27872  # video output port
+
+def main():
+    socket_in = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_out = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    socket_in.bind((HOST, IN_PORT))
+    socket_out.bind((HOST, OUT_PORT))
+
+    socket_out.listen()
+    out_conn, addr = socket_out.accept()
+    if out_conn:
+        print(f"Receiver connection from {addr}")
+
+    socket_in.listen()
+    in_conn, addr = socket_in.accept()
+    if in_conn:
+        print(f"Camera connection from {addr}")
+
+    # pass through
+
+if __name__ == "__main__":
+    OUT_PORT = int(sys.argv[1])
+    IN_PORT = int(sys.argv[2])
+    main()
